fc3cac29c2349bbd7a2777a3e96033cecf9b2712
arthurfdev <arthurfelipemendes44@gmail.com>
2024-04-19 16:07:58 -0300

    Bairro e Cidade Ok

---
+++ Bairro.java
@@ -0,0 +1,20 @@
+public class Bairro {
+    private Cidade cidade;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+}
