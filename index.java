79f9b9b384cfc3c140b95ecf584b9324be84ec7b
arthurfdev <arthurfelipemendes44@gmail.com>
2024-04-19 15:45:05 -0300

    index java commitado

---
+++ index.java
@@ -0,0 +1,34 @@
+public enum TipoLogradouro {
+    ALAMEDA,
+    AVENIDA,
+    MARGINAL,
+    RUA,
+    RODOVIA,
+    VIA,
+    VIELA,
+    TRAVESSA;
+
+    private TipoLogradouro() {
+    }
+}
+
+public class Logradouro {
+    private String nome;
+    private TipoLogradouro tipoLogradouro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoLogradouro getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(TipoLogradouro tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+}
